e6ea777451020b9efa04743866ed780cd22d09ed
guilloclaus <guillermo.j.ojeda.developer@gmail.com>
2023-10-10 22:39:39 -0300

    Creacion proyecto ejercicio forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { PageWrapperComponent } from './component/page-wrapper/page-wrapper.component';
+import { ToolbarComponent } from './component/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    PageWrapperComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
